ee49b89e79c8d8683176023107776db8f6592372
vosmiic <44092756+vosmiic@users.noreply.github.com>
2024-01-23 21:18:05 -0500

    fixed drawing pbm images

--- pi-eink-display-reciever/Program.cs
+++ pi-eink-display-reciever/Program.cs
@@ -1,7 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Device.Gpio;
-using System.Device.Gpio.Drivers;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.ImageSharp.PixelFormats;
@@ -30,7 +28,7 @@ namespace pi_eink_display_receiver
             newImage.Mutate(x => {
                 x.DrawLine(Color.Black, 1, new PointF(165, 50), new PointF(165, 100));
                 x.DrawLine(Color.Black, 1, new PointF(140, 75), new PointF(190, 75));
-                x.DrawImage(image, new Point(20, 50), PixelColorBlendingMode.Normal, PixelAlphaCompositionMode.Clear, 0);
+                x.DrawImage(image, new Point(20, 50), PixelColorBlendingMode.Normal, PixelAlphaCompositionMode.Src, 0);
             });
                 
             epd.Display(newImage);
